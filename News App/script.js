0c4b33db4055aa9fc405ba81560fa96ef8934c7b
ocampossoto <ocampossoto1@gmail.com>
2018-02-15 20:03:26 -0600

    Working

--- News App/script.js
+++ News App/script.js
@@ -1,40 +1,116 @@
 
-
+//main function to run
 function search(){
+    //check if anything is textboxes
+    //send to get_data function
     if(!$(topic).val() && !$(source).val()){
-        var data = get_data('abc-news');
-        console.log(data);
-        console.log('eh');
+        //do nothing
     }
     else if($(topic).val() && $(source).val()){
-        console.log('both');
+        get_data($(source).val(), $(topic).val());
     }
     else if($(source).val()){
-        console.log($(source).val());
+        get_data($(source).val(), null);
+
     }
     else if($(topic).val()){
-        console.log($(topic).val());
+        get_data(null, $(topic).val());
     }
 }
 
+//create the url to look up the news articles
+//submit and get results running the add to page function
+function get_data(source, topic){
+    var url = 'https://newsapi.org/v2/everything?'; //start of url
+    //check if source or topic is filled out or both
+    if(topic != null && source !== null){
+        url = url + 'q=' + topic;
+        url += 'sources='+ source;
+    }
+    else if(source !=null){
+        url += 'sources='+ source;
+    }
+    else if(topic != null){
+        url = url + 'q=' + topic;
 
-function get_data(source){
-    var url = 'https://newsapi.org/v2/everything?' +
-        'sources="'+ source +
-        'sortBy=' + topic +
-        '"&apiKey=f5a0b812dcdb4ad3aaab278da0d36b18';
-    var r = [];
+    }
+
+    //add key
+    url += '&apiKey=f5a0b812dcdb4ad3aaab278da0d36b18';
+
+    //request url
     var req = new Request(url);
+
+    //get request data
     fetch(req).then(function(response) {
+        //console.log(response); //log data (for debugging)
+
+        //get values of response
         var values = response.json();
+            //go through the response
+            //add to page
             values.then(function(x){
-                r = (x.articles);
-                console.log(r);
-                return r;
+                add_to_page(x.articles);
 
             })
         })
 
+}
+
+function add_to_page(x){
+    //console.log(x); //debugging
+
+    //check if we have any previous items
+    //if so delete them
+    if(document.getElementById('results') != null){
+        document.getElementById('results').remove();
+    }
+
+    //create outer div with id, for easy removal
+    var out = document.createElement('div');
+    out.setAttribute('id',"results")
+
+    //loop through results and add to page
+    x.forEach(function(value){
+        //don't add things without an image
+        if(value["urlToImage"]!=null){
+            // crate div article container
+            var div =document.createElement("div");
+            div.setAttribute('id',"article")
+
+            //create header
+            var head = document.createElement("h2");
+
+            //add title
+            var dat = document.createTextNode(value["title"]);
+            head.appendChild(dat);
+            div.appendChild(head)
+
+            //create publish date info
+            //add to page
+            var date = document.createElement('p');
+            var dat = document.createTextNode( 'Published At: '+ value['publishedAt']);
+            date.appendChild(dat);
+            div.appendChild(date);
+
+            //add link for image
+            var img_link = document.createElement('a');
+            img_link.href = value['url'];
+            img_link.target = "_blank";
+
+            //add image
+            var images = document.createElement("img");
+            images.src = value["urlToImage"];
+            img_link.appendChild(images);
+            div.appendChild(img_link);
+
+            //add to outer div
+            out.appendChild(div);
+        }
+
+    })
+    //add to page
+    document.body.appendChild(out);
 }
 
 
